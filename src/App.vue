42898ce25b4574e63e8a04e324beeb13b6ccf9e0
wingillis <win.gillis@gmail.com>
2016-06-30 15:33:31 -0400

    Build test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h4 style="text-align: center;">Timer Management System</h4>
-    <h1>{{audio.canPlayType()}}
+    <h1>{{audio.canPlayType('audio/ogg')}}
     <hr>
     <div v-sortable:cards="{animation: 150, handle: '.handle'}" style="margin: 0px auto;">
       <card v-for="(index, card) in cards" :card="card"></card>
