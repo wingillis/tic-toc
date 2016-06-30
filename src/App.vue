367a94df23f0926bce8658d060e2bec6ea0d11cb
wingillis <win.gillis@gmail.com>
2016-06-30 11:36:34 -0400

    Added alarm sound file from soundbible.com Added timer reset button and cancel timer commands

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@ export default {
         }
         return card
       })
-
+      this.$broadcast('reset')
     }
   }
 }
--- src/components/card.vue
+++ src/components/card.vue
@@ -13,6 +13,10 @@
 .mdl-card__supporting-text > h4 {
   margin-top: 0px;
 }
+
+#reset-button {
+  padding-bottom: 2px;
+}
 </style>
 
 <template lang="jade">
@@ -23,12 +27,16 @@
     button.mdl-button.mdl-js-button.mdl-button--icon(v-mdl, @click="notifyDelete")
       i.material-icons delete
   .mdl-card__supporting-text
-    h4 Time left: {{card.time}}
+    h4 Time left: {{strTime}}
+      button#reset-button.mdl-button.mdl-js-button.mdl-button--icon(v-mdl, v-if="canBeReset()", @click="resetTime")
+        i.material-icons cached
   .mdl-card__actions.mdl-card--border
     .action-bar(v-if="card.current")
-      button.mdl-button.mdl-js-button.mdl-js-ripple-effect.mdl-button--accent(v-mdl, @click="pause") Pause
+      button.mdl-button.mdl-js-button.mdl-js-ripple-effect.mdl-button--accent(v-mdl, @click="handleRunTimer")
+        span(v-if="timerRunning") Pause
+        span(v-else) Resume
       .mdl-layout-spacer
-      button.mdl-button.mdl-js-button.mdl-js-ripple-effect.mdl-button--raised.mdl-button--colored(v-mdl) Cancel
+      button.mdl-button.mdl-js-button.mdl-js-ripple-effect.mdl-button--raised.mdl-button--colored(v-mdl, @click="cancel") Cancel
     div(v-else)
       button.mdl-button.mdl-js-button.mdl-js-ripple-effect.mdl-button--raised.mdl-button--colored(v-mdl, @click="start") Start
 </template>
@@ -49,7 +57,9 @@ export default {
     // timeLeft is in milliseconds
     var data = {
       timeLeft: t,
-      timerRunning: false
+      originalTime: t,
+      timerRunning: false,
+      timerID: null
     }
     return data
   },
@@ -63,25 +73,61 @@ export default {
       this.$dispatch('delete', this.index)
     },
     start () {
+      var ctx = this
       this.card.current = true
       this.$dispatch('start', this.index)
+      this.timerRunning = true
+      this.timerID = setInterval(()=>{
+        this.timeLeft = this.timeLeft - 1000
+        if (this.timeLeft < 0) {
+          this.cancel()
+          // stop timer, reset time,
+          // call next timer
+        }
+      }, 1000)
       // start a 1s timer and store the timer id
     },
     stop () {
       // stop the timer function
+      if(this.timerID !== null) {
+        clearInterval(this.timerID)
+        this.timerID = null
+        this.timerRunning = false
+      }
+
     },
-    pause () {
-      this.stop() // stop the timer function
-      // pause now true
+    handleRunTimer () {
+      if (this.timerRunning) {
+        this.stop()
+      } else {
+        this.start()
+      }
     },
     cancel () {
+      // stop timer, reset elapsed time to original,
+      // set current to false
       this.stop()
-      // pause is now false
-      // reset amt of time to finish
+      this.resetTime()
+      this.card.current = false
+    },
+    canBeReset () {
+      return (!this.card.current && (this.timeLeft !== this.originalTime))
+    },
+    resetTime () {
+      this.timeLeft = this.originalTime
     }
   },
   computed: {
-
+    strTime() {
+      return numeral(this.timeLeft/1000).format('00:00:00')
+    }
+  },
+  events: {
+    'reset': function () {
+      if (!this.card.current) {
+        this.stop()
+      }
+    }
   }
 }
 </script>
